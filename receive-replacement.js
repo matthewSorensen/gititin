23d3158bb74f54dd6ab3e7b93133bada350e75ec
Matthew Sorensen <scholl.ma@gmail.com>
2011-08-15 00:29:25 -0700

    shaping up. Testing the project-section: project 101

--- receive-replacement.js
+++ receive-replacement.js
@@ -1,31 +1,34 @@
-function link(src,dest){ 
-    src.on('data',function(d){dest.write(d)});
-}
 var fs = require('fs'),
-    spawn = require('child_process').spawn;
-var repoPath = '/home/matthew/cs/repos',
-//Probably a much better place/way to create this!
-    randomName = 'repo-'+Math.round(1000*Math.random());
+    spawn = require('child_process').spawn,
+    submit = require('./submit.js').submit;
 
-process.chdir(repoPath);
+var tempPath   = '/home/matthew/cs/repos',
+    //Probably a much better place/way to create a random file name...
+    randomName = 'repo-'+Math.round(100000*Math.random());
 
-//syncronously make the new repository
-fs.mkdirSync(randomName,'0777');
-var init = spawn('git',['init',randomName,'--bare']);
-init.on('exit',function(){
-	// Once that's done, invoke 'git receive-pack <new repo>'
-	var receive = spawn('git',['receive-pack',randomName]);
-	// Link all of the streams:
-	var stdin = process.openStdin();
-	link(receive.stdout,process.stdout);
-	link(receive.stderr,process.stderr);
-	link(stdin,receive.stdin);
-	stdin.on('end',function(){receive.stdin.end();});
-	receive.on('exit',function(){
-		//After this point, stdout can't be written to.
-		require('./submit.js').submit(randomName);
+try {
+    process.chdir(tempPath);
+} catch (err){
+    console.error("gititin internal error: failed to set pwd");
+    process.exit(1);
+}
+// Make the temporary repository. 
+require('fs').mkdir(randomName,'0777',function(){
+	spawn('git',['init',randomName,'--bare']).on('exit',function(){
+		// Once that's done, invoke 'git receive-pack <new repo>'
+		var receive = spawn('git',['receive-pack',randomName]);
+		// Route stdout,stdin,stderr to/from git receive-pack
+		var stdin = process.openStdin(),
+		    link = function (src,dest){ 
+		    src.on('data',function(d){dest.write(d)});
+		};
+		link(receive.stdout,process.stdout);
+		link(receive.stderr,process.stderr);
+		link(stdin,receive.stdin);
+		stdin.on('end',function(){receive.stdin.end();});
+		receive.on('exit',function(){
+			//After this point, stdout can't be written to.
+			submit(randomName);
+		    });
 	    });
     });
-
-
-
