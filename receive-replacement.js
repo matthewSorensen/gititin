a2a352439cce9a21fc902bd6d38638c32f98a0e0
Matthew Sorensen <scholl.ma@gmail.com>
2011-08-18 00:13:54 -0700

    Much better way to get the repository - a little bit more git hacky, but doesn't fail - fixes #7

--- receive-replacement.js
+++ receive-replacement.js
@@ -1,46 +1,50 @@
 #!/usr/local/bin/node
+
 var tempPath   = '/tmp',
-    sourcePath = '/home/matthew/cs/gititin',
-    randomName = 'repo-'+Math.round(100000*Math.random());
+    sourcePath = '/home/matthew/cs/gititin';
+
 var fs = require('fs'),
     join = require('path').join,
     spawn = require(join(sourcePath,'spawn.js')),
     nat = require('nativeUtils');
+
 try {
     process.chdir(tempPath);
 } catch (err){
     console.error("gititin internal error: failed to set pwd");
     process.exit(1);
 }
+// Info that might be relevant in the {future} email module.
+var email = {workingDir: tempPath,
+	     sourcePath: sourcePath,
+	     repo: undefined,
+	     user: nat.usernameById(process.getuid()).replace(/\s|[^A-Za-z0-9]/,'')},
+    time = new Date();
+email.repo = [email.user, time.getMonth()+1, time.getDate(), time.getYear()].join('-');
 // Make the temporary repository. 
-require('fs').mkdir(randomName,'0777',function(){
-	spawn.spawn('git',['init',randomName,'--bare'],function(){
+require('fs').mkdir(email.repo,'0777',function(){
+	spawn.spawn('git',['init','--template='+join(sourcePath,'template-repo'),email.repo],function(){
 		// Once that's done, invoke 'git receive-pack <new repo>'
-		var receive = spawn.old_spawn('git',['receive-pack',randomName]);
+		var receive = spawn.old_spawn('git',['receive-pack',email.repo]);
 		// Route stdout,stdin,stderr to/from git receive-pack
 		process.stdin.resume();
 		receive.stdout.pipe(process.stdout);
 		receive.stderr.pipe(process.stderr);
 		process.stdin.pipe(receive.stdin);
-		
 		receive.on('exit',function(){
 			// Once the real git receive-pack has terminated, the replacement probably should.
 			// However, there's still work to do, so we fork a child, which daemonizes itself once it's got the data.
 			fork(function(){
 				process.exit(0);
 			    },function(){
-				spawn.spawn('git',['clone','-b','master','-l',randomName,randomName+'-full'],function(){
-					var opts = {workingDir: tempPath, 
-						    repo: randomName, 
-						    sourceDir: sourcePath,
-						    user: nat.usernameById(process.getuid())
-					};
-					daemonize();
-					require(join(sourcePath,'submit.js')).submit(opts);
+				daemonize();
+				spawn.spawn('tar',['-zcvf',email.repo+'.tar.gz',email.repo],function(){
+					spawn.spawn('rm',['-rf',email.repo],function(){
+						//The farthest flung point of control flow!
+					    });
 				    });
 			    });
 		    });
-		
 	    });
     });
 function fork(parent,child){
